cae390c06d3886a6cd4c5299d68c10d828b7b411
Gopi Krishna Muktevi <muktevigk@handbookinc.com>
2018-05-17 16:06:09 +0530

    Intial Commit

---
+++ app/components/sample_component.js
@@ -0,0 +1,6 @@
+var app = angular.module("myApp");
+
+app.component('sample', {
+    templateUrl: 'view1/view1.html',
+
+})
---
+++ app/controller/SampleController.js
@@ -0,0 +1,5 @@
+var app = angular.module("myApp.view1");
+
+app.controller("SampleController", ['$scope', function ($scope) {
+    console.log("Inside Sample Controller");
+}]);
